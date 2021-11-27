3677cbc16f54ada90f80c311038ecc12acdbc6c3
Максим Сергеев <nuparu19988991@gmail.com>
2021-11-27 19:25:29 +0300

    mongo

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from pymongo import collection
 import config
 import telebot
 import logging
 import data
+import notesDB
 
 
 #логирование
@@ -11,8 +13,6 @@ telebot.logger.setLevel(logging.DEBUG)
 #создаем экземпляр бота
 bot = telebot.TeleBot(config.TOKEN)
 
-#временный контейнер для списка заметок
-list = []
 
 #создаем клавиатуру
 markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -41,44 +41,41 @@ def msg_reply(msg):
             answer = bot.send_message(msg.chat.id, f'Что добавляем?', reply_markup=emptyMarkup)
             bot.register_next_step_handler(answer, add_item)
         case data.Answer.Delete:
-            if (len(list) == 0):
+            notes = notesDB.GetNotes()
+            if (notes.count() == 0):
                 bot.send_message(msg.chat.id, f'Нет заметок', reply_markup=markup)
             else:
                 listMarkup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-                for item in list:
-                    listMarkup.add(telebot.types.KeyboardButton(item))
+                for item in notes:
+                    listMarkup.add(telebot.types.KeyboardButton(item['note']))
                 answer = bot.send_message(msg.chat.id, f'Что удаляем?', reply_markup=listMarkup)
                 bot.register_next_step_handler(answer, del_item)
         case data.Answer.Show:
-            if (len(list) == 0):
+            notes = notesDB.GetNotes()
+            if (notes.count() == 0):
                 bot.send_message(msg.chat.id, f'Нет заметок', reply_markup=markup)
             else:
-                listed = "\n".join(list)
+                listed = ""
+                for n in notes:
+                    listed += n['note'] + '\n'
                 bot.send_message(msg.chat.id, f'Список заметок:\n{listed}', reply_markup=markup)
         case data.Answer.Clear:
-            list.clear()
+            notesDB.ClearNotes()
             bot.send_message(msg.chat.id, f'Заметки очищены', reply_markup=markup)
         case _:
             bot.send_message(msg.chat.id, f'Неизвестная команда', reply_markup=markup)
 
 def add_item(msg):
-    lower = msg.text.lower()
-    isAlreadyExist = False
-    for i in list:
-        if i.lower() == lower:
-            isAlreadyExist = True
+    isAlreadyExist = notesDB.NoteExist(msg.text)
 
     if isAlreadyExist:
         bot.send_message(msg.chat.id, f'{msg.text} уже есть в списке', reply_markup=markup)
     else:
-        list.append(msg.text)
+        notesDB.InsertNote(msg.text)
         bot.send_message(msg.chat.id, f'Добавил {msg.text} в список', reply_markup=markup)
 
 def del_item(msg):
-    lower = msg.text.lower()
-    for i in list:
-        if i.lower() == lower:
-            list.remove(i)
+    notesDB.DelNote(msg.text)
 
     bot.send_message(msg.chat.id, f'Удалил {msg.text} из списка', reply_markup=markup)
 
--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 TOKEN = '2112804611:AAEWELKqmaIscx7ROBLK8BwGrd2EfqQzmiE'
+MONGO_CONN_STRING = "mongodb+srv://genshindb:pp6c6z49@cluster0.xalem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
